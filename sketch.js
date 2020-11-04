aad47b056ef641ab3bea8dd2948ba81121ef8ef8
surajkumar0419 <73117869+surajkumar0419@users.noreply.github.com>
2020-11-04 18:58:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,73 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground;
+var box1,box2,box3;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	engine = Engine.create();
+	world = engine.world;
+
+	rectMode(CENTER);
+	
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG);
+	packageSprite.scale=0.2;
+	
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+	
+	box1=createSprite(400,650,200,20);
+	box1.shapeColor=color("red");
+	box2=createSprite(310,590,20,100);
+	box2.shapeColor=color("red");
+	box3=createSprite(490,590,20,100);
+	box3.shapeColor=color("red");
+	
+	box1base=Bodies.rectangle(410,650,200,20,{isStatic:true});
+	World.add(world,box1base);
+
+	box2left=Bodies.rectangle(310,590,20,100,{isStatic:true});
+	World.add(world,box2left);
+
+	box3right=Bodies.rectangle(490,590,20,100,{isStatic:true});
+	World.add(world,box3right);
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, packageBody);
+
+}
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  Engine.update(engine);
+  packageSprite.x= packageBody.position.x;
+  packageSprite.y= packageBody.position.y;
+
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+  Matter.Body.setStatic(packageBody,false);
+	// Look at the hints in the document and understand how to make the package body fall only on
+	}
+  }
